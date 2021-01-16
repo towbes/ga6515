4951d440b0428a7b82762b8ccdf9fc9249216a15
hank <hank>
2021-01-16 20:04:31 +0900

    update big o to check 10 sets of inputs

--- bigOcheck.go
+++ bigOcheck.go
@@ -18,7 +18,7 @@ func trackTime(start time.Time, result float64, name string) {
 //f(n)
 func fofn(n float64) float64 {
 
-	result := (math.Pow(n, 2)) / math.Log(n)
+	result := math.Pow(n, 0.1)
 	defer trackTime (time.Now(), result, "F of n")
 
 	return result
@@ -27,15 +27,27 @@ func fofn(n float64) float64 {
 //g(n)
 func gofn(n float64) float64 {
 
-	result := n * math.Pow(math.Log(n), 2)
+	result := math.Pow(math.Log(n), 10)
 	defer trackTime (time.Now(), result, "G of n")
 
 	return result
 }
 
 func main() {
-	input := float64(99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999)
 
-	fofn(input)
-	gofn(input)
+	inputs := make([]float64, 10)
+	timesten := float64(1)
+
+	for i := 0; i < 10; i++ {
+		inputs[i] = float64(1) * math.Pow(10, timesten)
+		timesten += 3 
+	}
+
+	for i := 0; i < 10; i++ {
+		x := fofn(inputs[i])
+		y := gofn(inputs[i])
+		difference := math.Abs(x - y)
+		log.Printf("Difference: %v", difference)
+	}
+
 }
